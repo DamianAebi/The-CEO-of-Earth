f62376f5d16d125428ba82c160729061c60ee79b
Damian <damian@tie-international.com>
2023-10-04 11:25:43 +0200

    fully done with read more / read less functionality

--- web/main_scripts.js
+++ web/main_scripts.js
@@ -12,11 +12,38 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // function to change height of container and make space
+    // functions to expand and shrink height of container
     function enlargeContainer() {
+        // remove maxheight of Parentcontainer so it fits text
         this.parentElement.style.maxHeight = "none";
+        this.parentElement.style.overflowY = "scroll";
+
+        // don't display CEO image and subtitle anymore
         this.parentElement.nextElementSibling.style.display = "none";
-        this.style.display = "none";
+        this.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "none";
+
+        // Change button text
+        this.children[0].innerHTML = "Weniger lesen...";
+
+        // make it so "read more" banner is part of document flow again so it's not on top of text
+        this.style.position = "relative";
+
+        this.removeEventListener("click", enlargeContainer);
+        this.addEventListener("click", shrinkContainer);
+    }
+
+    function shrinkContainer() {
+        // Same as function above but in reverse
+
+        this.parentElement.style.maxHeight = "230px";
+        this.parentElement.scrollTop = 0;
+        this.parentElement.style.overflowY = "hidden";
+        this.parentElement.nextElementSibling.style.display = "block";
+        this.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "block";
+        this.children[0].innerHTML = "Mehr lesen...";
+        this.style.position = "absolute";
+        this.removeEventListener("click", shrinkContainer);
+        this.addEventListener("click", enlargeContainer);
     }
 
 });
